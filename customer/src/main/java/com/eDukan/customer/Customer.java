4216f4314d3392d022fac92e78fd48047a8dedc3
Shehroz Ali <sshehroza13@gmail.com>
2022-09-12 15:13:08 +0500

    Annotated with @Entity, @AllArgsContructor, @NoArgsConstructor, @Id, @SequenceGenerator and @GeneratedValue

--- customer/src/main/java/com/eDukan/customer/Customer.java
+++ customer/src/main/java/com/eDukan/customer/Customer.java
@@ -1,11 +1,22 @@
 package com.eDukan.customer;
 
+import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
     private Integer id;
     private String firstname;
     private String lastname;
