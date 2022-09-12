2763798aa977a22188dbc4b4a8fe30fbd235e852
Shehroz Ali <sshehroza13@gmail.com>
2022-09-12 13:29:53 +0500

    Used builder pattern to build Customer entity

--- customer/src/main/java/com/eDukan/customer/CustomerService.java
+++ customer/src/main/java/com/eDukan/customer/CustomerService.java
@@ -1,4 +1,7 @@
 package com.eDukan.customer;
 
 public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder().firstname(request.firstname()).lastname(request.lastname()).email(request.email()).build();
+    }
 }
