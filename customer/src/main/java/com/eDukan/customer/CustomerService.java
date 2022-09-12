5eebc8098c69ad3ab62bc0366056d4e5320a53a0
Shehroz Ali <sshehroza13@gmail.com>
2022-09-12 15:54:37 +0500

    CustomerRepository dependency injected Saved customer: customerRepository.save(customer)

--- customer/src/main/java/com/eDukan/customer/CustomerService.java
+++ customer/src/main/java/com/eDukan/customer/CustomerService.java
@@ -1,7 +1,8 @@
 package com.eDukan.customer;
 
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder().firstname(request.firstname()).lastname(request.lastname()).email(request.email()).build();
+        customerRepository.save(customer);
     }
 }
