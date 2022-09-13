cca60a832da5a11b3127644c4fbe9e6e2aad943f
Shehroz Ali <sshehroza13@gmail.com>
2022-09-13 12:40:59 +0500

    Refactored to FraudCheckHistoryRepository

--- fraud/src/main/java/com/eDukan/fraud/FraudCheckRepository.java
+++
@@ -1,6 +0,0 @@
-package com.eDukan.fraud;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FraudCheckRepository extends JpaRepository<FraudCheckHistory, Integer> {
-}
