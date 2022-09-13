ae5c3664d616c571ad7af42c74402c4c6870f5f4
Shehroz Ali <sshehroza13@gmail.com>
2022-09-13 13:36:43 +0500

    Added todo comment Injected RestTemplate

--- customer/src/main/java/com/eDukan/customer/CustomerService.java
+++ customer/src/main/java/com/eDukan/customer/CustomerService.java
@@ -2,16 +2,19 @@ package com.eDukan.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder().firstname(request.firstname()).lastname(request.lastname()).email(request.email()).build();
         // todo: check if email is valid
         // todo: check if email is already taken
         // todo: check if customer is fraudulent
+        // todo: send notification
 
         customerRepository.save(customer);
     }
