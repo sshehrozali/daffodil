60ee6d8ce0eab617720fee0ed13f2f01b0155e07
Shehroz Ali <sshehroza13@gmail.com>
2022-09-13 12:23:51 +0500

    Refactored

--- customer/src/main/java/com/eDukan/customer/CustomerController.java
+++ customer/src/main/java/com/eDukan/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package com.eDukan.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
-public record CustomerController(CustomerService customerService) {
-
+@AllArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("New Customer Registration {}", customerRegistrationRequest);
--- customer/src/main/java/com/eDukan/customer/CustomerService.java
+++ customer/src/main/java/com/eDukan/customer/CustomerService.java
@@ -1,6 +1,12 @@
 package com.eDukan.customer;
 
-public record CustomerService(CustomerRepository customerRepository) {
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder().firstname(request.firstname()).lastname(request.lastname()).email(request.email()).build();
         customerRepository.save(customer);
