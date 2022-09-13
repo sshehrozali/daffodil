4a9d3baedd41e3e2ff748f332909f32cc5abcb5d
Shehroz Ali <sshehroza13@gmail.com>
2022-09-13 14:06:47 +0500

    Added logging

--- fraud/src/main/java/com/eDukan/fraud/FraudCheckHistoryController.java
+++ fraud/src/main/java/com/eDukan/fraud/FraudCheckHistoryController.java
@@ -1,6 +1,7 @@
 package com.eDukan.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudCheckHistoryController {
     private final FraudCheckHistoryService fraudCheckHistoryService;
 
     @GetMapping(path = "{customerId}")
     public FraudCheckHistoryResponse isFraudulent(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulent = fraudCheckHistoryService.isFraudulentCustomer(customerId);
+        log.info("Fraud Check Request for Customer: {}", customerId);
         return new FraudCheckHistoryResponse(isFraudulent);
     }
 }
