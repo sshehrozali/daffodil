757709ca5f60878946c9c9d0b3861179bf1714a0
Shehroz Ali <sshehroza13@gmail.com>
2022-09-23 14:22:13 +0500

    CustomerService: Added comment

--- customer/src/main/java/com/eDukan/customer/CustomerService.java
+++ customer/src/main/java/com/eDukan/customer/CustomerService.java
@@ -1,16 +1,25 @@
 package com.eDukan.customer;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
+
 @Service
 @AllArgsConstructor
+@Slf4j
 public class CustomerService {
     private final CustomerRepository customerRepository;
     private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
-        Customer customer = Customer.builder().firstname(request.firstname()).lastname(request.lastname()).email(request.email()).build();
+        Customer customer = Customer.builder()
+                .firstname(request.firstname())
+                .lastname(request.lastname())
+                .email(request.email())
+                .createdAt(LocalDateTime.now())
+                .build();
         customerRepository.saveAndFlush(customer);
 
         // todo: check if email is valid
@@ -19,8 +28,13 @@ public class CustomerService {
         FraudCheckHistoryResponse fraudCheckHistoryResponse = restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckHistoryResponse.class, customer.getId());
         if (fraudCheckHistoryResponse.isFraudster()) {
+            // If Customer is Fraudster -> Throw exception
             throw new IllegalStateException("Customer is Fraudster");
+        } else {
+            // send notification
+            NotificationRequest notificationRequest = new NotificationRequest(customer.getEmail(), customer.getCreatedAt());
+            NotificationResponse response = restTemplate.postForObject("http://NOTIFICATION/api/v1/notification", notificationRequest, NotificationResponse.class);
+            log.info("Notification sent status: " + response.isSent());
         }
-        // todo: send notification
     }
 }
