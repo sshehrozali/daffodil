bec66714718d80dabf761b172163cae3288b1a4d
Shehroz Ali <sshehroza13@gmail.com>
2022-09-23 13:05:41 +0500

    Notification: Implemented entity fields Notification: Added @Entity, @AllArgsConstructor, @NoArgsContructor annotations

--- notification/src/main/java/com/eDukan/notification/Notification.java
+++ notification/src/main/java/com/eDukan/notification/Notification.java
@@ -1,4 +1,21 @@
 package com.eDukan.notification;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Notification {
+    @Id
+    @SequenceGenerator(name = "notification_id_sequence", sequenceName = "notification_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "notification_id_sequence")
+    private Integer id;
+    private String customerEmail;
+    private LocalDateTime registeredAt;
+    private LocalDateTime sentAt;
 }
