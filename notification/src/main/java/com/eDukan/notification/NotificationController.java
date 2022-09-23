bffdd4593790539994444283842c48f5f09f8152
Shehroz Ali <sshehroza13@gmail.com>
2022-09-23 14:29:12 +0500

    NotificationController: Annotated publishNotification() method with @PostMapping

--- notification/src/main/java/com/eDukan/notification/NotificationController.java
+++ notification/src/main/java/com/eDukan/notification/NotificationController.java
@@ -2,6 +2,7 @@ package com.eDukan.notification;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
     private final NotificationService notificationService;
 
+    @PostMapping
     public NotificationResponse publishNotification(@RequestBody NotificationRequest request) {
         log.info("New Notification: " + request.customerEmail() + "registered at " + request.registeredAt());
         return notificationService.send(request);
