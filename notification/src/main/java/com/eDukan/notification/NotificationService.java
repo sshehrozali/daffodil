3cb6f1c606193c2c39a72c4eec20dae04e0c249f
Shehroz Ali <sshehroza13@gmail.com>
2022-09-23 13:45:54 +0500

    NotificationService: Injected NotificationRepository via constructor-based injection NotificationService: Added @AllArgsConstructor annotation

--- notification/src/main/java/com/eDukan/notification/NotificationService.java
+++ notification/src/main/java/com/eDukan/notification/NotificationService.java
@@ -1,10 +1,20 @@
 package com.eDukan.notification;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
+@AllArgsConstructor
 public class NotificationService {
+    private final NotificationRepository notificationRepository;
     public void send(NotificationRequest request) {
+        Notification notification = Notification.builder()
+                .customerEmail(request.customerEmail())
+                .registeredAt(request.registeredAt())
+                .sentAt(LocalDateTime.now())
+                .build();
 
     }
 }
