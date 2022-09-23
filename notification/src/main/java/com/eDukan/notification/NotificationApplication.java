47d6bd9560f203f337f1fb47526cbf4166ef0cc5
Shehroz Ali <sshehroza13@gmail.com>
2022-09-23 12:32:54 +0500

    NotificationApplication: Added @EnableEurekaClient annotation

--- notification/src/main/java/com/eDukan/notification/NotificationApplication.java
+++ notification/src/main/java/com/eDukan/notification/NotificationApplication.java
@@ -2,8 +2,10 @@ package com.eDukan.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
