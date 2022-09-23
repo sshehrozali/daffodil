02fd9fccac2e56bb5ea59457b65d0423516f72d2
Shehroz Ali <sshehroza13@gmail.com>
2022-09-23 12:59:12 +0500

    NotificationService: Added @Service annotation

--- notification/src/main/java/com/eDukan/notification/NotificationService.java
+++ notification/src/main/java/com/eDukan/notification/NotificationService.java
@@ -1,4 +1,10 @@
 package com.eDukan.notification;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificationService {
+    public void send(NotificationRequest request) {
+
+    }
 }
