a44c1b7fe81f7c2b3eacfb3b7e26e8aff0abb433
Shehroz Ali <sshehroza13@gmail.com>
2022-09-23 12:52:26 +0500

    NotificationController: Implemented publishNotifification() method NotificationController: Added @Slf4j, @AllAgrsContructor annotations

--- notification/src/main/java/com/eDukan/notification/NotificationController.java
+++ notification/src/main/java/com/eDukan/notification/NotificationController.java
@@ -1,4 +1,20 @@
 package com.eDukan.notification;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/notification")
+@AllArgsConstructor
+@Slf4j
 public class NotificationController {
+    private final NotificationService notificationService;
+
+    public void publishNotification(@RequestBody NotificationRequest request) {
+        log.info("New Notification: " + request.customerEmail() + "registered at " + request.registeredAt());
+        notificationService.send(request);
+    }
 }
