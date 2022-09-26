836ebb5a1b9e64e80bf70c4024484bc5579d62de
Shehroz Ali <sshehroza13@gmail.com>
2022-09-26 13:34:56 +0500

    APIGatewayApplication: Created main() method APIGatewayApplication: Annotated the class with @SpringBootApplication

--- api-gateway/src/main/java/com/eDukan/apigateway/APIGatewayApplication.java
+++ api-gateway/src/main/java/com/eDukan/apigateway/APIGatewayApplication.java
@@ -1,4 +1,11 @@
 package com.eDukan.apigateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class APIGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(APIGatewayApplication.class, args);
+    }
 }
