940fd782f7c3d1627807f42a3190e6ee2d3de5f8
Shehroz Ali <sshehroza13@gmail.com>
2022-09-26 13:37:45 +0500

    APIGatewayApplication: Added @EnableEurekaClient annotation

--- api-gateway/src/main/java/com/eDukan/apigateway/APIGatewayApplication.java
+++ api-gateway/src/main/java/com/eDukan/apigateway/APIGatewayApplication.java
@@ -2,8 +2,10 @@ package com.eDukan.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class APIGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(APIGatewayApplication.class, args);
