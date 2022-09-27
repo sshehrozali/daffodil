679e9ef1a85ccedbfa1e544a0703c5fa10d32f2a
Shehroz Ali <sshehroza13@gmail.com>
2022-09-27 13:56:01 +0500

    APIGatewayApplication: Added @EnableDiscoveryClient annotation

--- api-gateway/src/main/java/com/eDukan/apigateway/APIGatewayApplication.java
+++ api-gateway/src/main/java/com/eDukan/apigateway/APIGatewayApplication.java
@@ -2,10 +2,12 @@ package com.eDukan.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class APIGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(APIGatewayApplication.class, args);
