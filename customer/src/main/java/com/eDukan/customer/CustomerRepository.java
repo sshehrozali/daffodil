b6d7bbfd997b7858ee3a3493291422dcce3a184b
Shehroz Ali <sshehroza13@gmail.com>
2022-09-28 15:05:02 +0500

    CustomerRepository: Implemented findByEmail() method

--- customer/src/main/java/com/eDukan/customer/CustomerRepository.java
+++ customer/src/main/java/com/eDukan/customer/CustomerRepository.java
@@ -3,4 +3,5 @@ package com.eDukan.customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Customer findByEmail(String email);
 }
